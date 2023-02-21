89d5f48d0dc7639ce188d50ca0a6197cbb1eefbf
KyleDeveaux <kyledeveaux@gmail.com>
2023-02-20 23:05:38 -0500

    feat(fcfs-algorithm): complete code

--- FCFS.js
+++ FCFS.js
@@ -1,37 +1,59 @@
+// Define the process data
 const processes = [
-    { name: "P1", cpuBurst: [6, 5, 7, 8, 6, 5, 9, 7, 8], ioTime: [41, 42, 40, 38, 44, 41, 31, 43] },
-    { name: "P2", cpuBurst: [9, 7, 8, 12, 9, 11, 8, 12, 7, 6], ioTime: [24, 21, 36, 26, 31, 28, 21, 13, 11] },
-    { name: "P3", cpuBurst: [7, 8, 12, 6, 8, 9, 6, 4, 16], ioTime: [21, 25, 29, 26, 33, 22, 24, 29] },
-    { name: "P4", cpuBurst: [5, 7, 14, 4, 9, 10, 11, 5, 3], ioTime: [35, 41, 45, 51, 61, 54, 82, 77] },
-    { name: "P5", cpuBurst: [6, 7, 5, 9, 8, 5, 7, 4, 3], ioTime: [33, 44, 42, 37, 46, 41, 31, 43] },
-    { name: "P6", cpuBurst: [8, 12, 11, 12, 9, 19, 10, 6, 3, 4], ioTime: [24, 21, 36, 26, 31, 28, 21, 13, 11] },
-    { name: "P7", cpuBurst: [7, 3, 12, 8, 4, 6, 12, 10], ioTime: [46, 41, 42, 21, 32, 19, 33] },
-    { name: "P8", cpuBurst: [6, 7, 8, 9, 10, 11, 8], ioTime: [14, 33, 51, 63, 87, 74] },
-    { name: "P9", cpuBurst: [4, 5, 6, 4, 5, 6, 4, 5, 6], ioTime: [32, 40, 29, 21, 44, 24, 31, 33] }
+    { id: "P1", arrivalTime: 0, bursts: [6, 41, 5, 42, 7, 40, 8, 38, 6, 44, 5, 41, 9, 31, 7, 43, 8] },
+    { id: "P2", arrivalTime: 0, bursts: [9, 24, 7, 21, 8, 36, 12, 26, 9, 31, 11, 28, 8, 21, 12, 13, 7, 11, 6] },
+    { id: "P3", arrivalTime: 0, bursts: [7, 21, 8, 25, 12, 29, 6, 26, 8, 33, 9, 22, 6, 24, 4, 29, 16] },
+    { id: "P4", arrivalTime: 0, bursts: [5, 35, 7, 41, 14, 45, 4, 51, 9, 61, 10, 54, 11, 82, 5, 77, 3] },
+    { id: "P5", arrivalTime: 0, bursts: [6, 33, 7, 44, 5, 42, 9, 37, 8, 46, 5, 41, 7, 31, 4, 43, 3] },
+    { id: "P6", arrivalTime: 0, bursts: [8, 24, 12, 21, 11, 36, 12, 26, 9, 31, 19, 28, 10, 21, 6, 13, 3, 11, 4] },
+    { id: "P7", arrivalTime: 0, bursts: [7, 46, 3, 41, 12, 42, 8, 21, 4, 32, 6, 19, 12, 33, 10] },
+    { id: "P8", arrivalTime: 0, bursts: [6, 14, 7, 33, 8, 51, 9, 63, 10, 87, 11, 74, 8] },
+    { id: "P9", arrivalTime: 0, bursts: [4, 32, 5, 40, 6, 29, 4, 21, 5, 44, 6, 24, 4, 31, 5, 33, 6] },
   ];
+  
+  // Sort the processes based on their arrival time
+  processes.sort((a, b) => a.arrivalTime - b.arrivalTime);
+  
+  // Define variables to store the metrics
+  let cpuUtilization = 0;
+  let responseTime = 0;
+  let waitTime = 0;
+  let turnaroundTime = 0;
+// Define a variable to store the current time
+let currentTime = 0;
 
-const fcfsScheduler = (processes) => {
-  console.log("Starting FCFS scheduler...");
-  let currentTime = 0;
-  let completionTimes = [];
-  let waitingTimes = [];
+// Loop through the processes
+for (let i = 0; i < processes.length; i++) {
+  const process = processes[i];
 
-  for (let i = 0; i < processes.length; i++) {
-    const process = processes[i];
-    console.log(`Executing ${process.name}...`);
-    currentTime += process.cpuBurst.reduce((a, b) => a + b, 0);
-    completionTimes.push({ name: process.name, completionTime: currentTime });
-    waitingTimes.push({
-      name: process.name,
-      waitingTime: currentTime - process.cpuBurst[0],
-    });
-  }
+  // Calculate the start time of the process
+  const startTime = Math.max(currentTime, process.arrivalTime);
 
-  console.log("FCFS scheduling complete!");
-  console.log("Task completion times:");
-  console.log(completionTimes);
-  console.log("Task waiting times:");
-  console.log(waitingTimes);
-};
+  // Update the response time
+  responseTime += startTime - process.arrivalTime;
 
-fcfsScheduler(processes);
+  // Update the CPU utilization
+  cpuUtilization += process.bursts.reduce((acc, burst) => acc + burst, 0);
+
+  // Update the current time
+  currentTime = startTime + process.bursts.reduce((acc, burst) => acc + burst, 0);
+
+  // Calculate the wait time
+  waitTime += startTime - process.arrivalTime;
+
+  // Calculate the turnaround time
+  turnaroundTime += currentTime - process.arrivalTime;
+}
+
+// Calculate the CPU utilization
+cpuUtilization = cpuUtilization / currentTime;
+
+// Calculate the average response time, wait time, and turnaround time
+responseTime /= processes.length;
+waitTime /= processes.length;
+turnaroundTime /= processes.length;
+
+// Print the results
+console.log("Average Response Time: " + responseTime);
+console.log("Average Wait Time: " + waitTime);
+console.log("Average Turnaround Time: " + turnaroundTime);
